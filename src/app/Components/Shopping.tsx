cc895b4b5d57de9306be59370b4e278590f6e503
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-30 17:59:00 +0500

    worked is very hard

--- src/app/Components/Shopping.tsx
+++ src/app/Components/Shopping.tsx
@@ -1,3 +1,224 @@
+// import React from 'react'
+// import Image from 'next/image'
+
+// const Shopping = () => {
+//     return (
+//         <div>
+//             {/* first side started */}
+//             <div className='bg-[#F6F5FF] py-10 px-6 md:py-[140px] md:px-[200px]'>
+//                 <h1 className='font-JosefinSans text-[24px] text-[#101750] font-semibold md:text-[29px] text-3xl'>
+//                     Shopping Page
+//                 </h1>
+//                 <div className='flex flex-wrap gap-2 py-4'>
+//                     <div>
+//                         <p className='text-[14px] md:text-[16px] font-light text-[#000000]'>Home ,</p>
+//                     </div>
+//                     <div>
+//                         <p className='text-[14px] md:text-[16px] font-light text-[#000000]'>Page ,</p>
+//                     </div>
+//                     <div>
+//                         <p className='text-[14px] md:text-[16px] text-[#FB2E86]'>Blog Page</p>
+//                     </div>
+//                 </div>
+//             </div>
+//             {/* first side completed  */}
+
+//             {/*second part started  */}
+//             <div className='h-[1800px] w-full py-[90px] px-[380px]'>
+//                 <div>
+//                     <Image
+//                         src="/page sort by.png"
+//                         alt="Phone Icon"
+//                         width={1171}
+//                         height={44}
+//                         className="max-w-full h-auto"
+//                     />
+//                 </div>
+//                 {/* second */}
+//                 <div className='flex gap-14 py-[90px]'>
+//                     <div>
+//                         <Image
+//                             src="/Ecommerce accesories.png "
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                     <div>
+
+//                         {/* c:\Users\wajiz.pk\Downloads\Ecommerce accesories (1).png */}
+//                         <Image
+//                             src="/Ecommerce accesories (1).png "
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                     <div>
+//                         <Image
+//                             src="/Ecommerce accesories.png"
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                     <div>
+//                         <Image
+//                             src="/Ecommerce accesories (3).png"
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                 </div>
+
+
+//                 {/* second shop  */}
+//                 <div className='flex gap-14 py-[90px]'>
+//                     <div>
+//                         <Image
+//                             src="/Ecommerce accesories (7).png "
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                     <div>
+
+//                         {/* c:\Users\wajiz.pk\Downloads\Ecommerce accesories (1).png */}
+//                         <Image
+//                             src="/Ecommerce accesories (8).png"
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                     <div>
+//                         <Image
+//                             src="/Ecommerce accesories (9).png"
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                     <div>
+//                         <Image
+//                             src="/Ecommerce accesories (10).png"
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                 </div>
+//                 {/* third shop */}
+//                 <div className='flex gap-14 py-[90px]'>
+//                     <div>
+//                         <Image
+//                             src="/Ecommerce accesories (4).png "
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                     <div>
+
+//                         {/* c:\Users\wajiz.pk\Downloads\Ecommerce accesories (1).png */}
+//                         <Image
+//                             src="/Ecommerce accesories (5).png "
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                     <div>
+//                         <Image
+//                             src="/Ecommerce accesories (6).png"
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                     <div>
+//                         <Image
+//                             src="/Ecommerce accesories (3).png"
+//                             alt="Phone Icon"
+//                             width={300}
+//                             height={363}
+//                             className="max-w-full h-auto"
+//                         />
+//                     </div>
+//                 </div>
+//             </div>
+
+//             {/* last  */}
+//             {/*  <div className='py-[80px] px-[580px]'>
+//                     <Image
+//                         src="/imgesss.png"
+//                         alt="Phone Icon"
+//                         width={904}
+//                         height={200}
+//                         className="max-w-full h-auto"
+//                     />
+//                 </div>
+//                 <div> <Image
+//                     src="/footer.png"
+//                     alt="Phone Icon"
+//                     width={2120}
+//                     height={532}
+//                     className="max-w-full h-auto"
+//                 />
+//                 </div> */}
+//             <div className='h-[220px] w-full'>
+
+//                 <div className='py-[70px] px-[600px] '>
+//                     <Image
+//                         src="/imgesss.png"
+//                         alt="Phone Icon"
+//                         width={904}
+//                         height={200}
+//                         className="max-w-full h-auto"
+//                     />
+//                 </div>
+
+//             </div>
+
+
+
+//             <div className='h-[532px] w-full'>
+//                 <Image
+//                     src="/footer.png"
+//                     alt="Phone Icon"
+//                     width={2120}
+//                     height={532}
+//                     className="max-w-full h-auto"
+//                 />
+//             </div>
+
+
+
+
+//         </div>
+//     )
+// }
+
+// export default Shopping
+
+
+
+
+
+
 import React from 'react'
 import Image from 'next/image'
 
@@ -10,206 +231,69 @@ const Shopping = () => {
                     Shopping Page
                 </h1>
                 <div className='flex flex-wrap gap-2 py-4'>
-                    <div>
-                        <p className='text-[14px] md:text-[16px] font-light text-[#000000]'>Home ,</p>
-                    </div>
-                    <div>
-                        <p className='text-[14px] md:text-[16px] font-light text-[#000000]'>Page ,</p>
-                    </div>
-                    <div>
-                        <p className='text-[14px] md:text-[16px] text-[#FB2E86]'>Blog Page</p>
-                    </div>
+                    <p className='text-[14px] md:text-[16px] font-light text-[#000000]'>Home ,</p>
+                    <p className='text-[14px] md:text-[16px] font-light text-[#000000]'>Page ,</p>
+                    <p className='text-[14px] md:text-[16px] text-[#FB2E86]'>Blog Page</p>
                 </div>
             </div>
             {/* first side completed  */}
 
             {/*second part started  */}
-            <div className='h-[1800px] w-full py-[90px] px-[380px]'>
+            <div className='w-full py-10 px-6 md:py-[90px] md:px-[10%]'>
                 <div>
                     <Image
                         src="/page sort by.png"
                         alt="Phone Icon"
                         width={1171}
                         height={44}
-                        className="max-w-full h-auto"
+                        className="w-full h-auto"
                     />
                 </div>
                 {/* second */}
-                <div className='flex gap-14 py-[90px]'>
-                    <div>
-                        <Image
-                            src="/Ecommerce accesories.png "
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
-                    <div>
-
-                        {/* c:\Users\wajiz.pk\Downloads\Ecommerce accesories (1).png */}
-                        <Image
-                            src="/Ecommerce accesories (1).png "
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src="/Ecommerce accesories.png"
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src="/Ecommerce accesories (3).png"
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
+                <div className='grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-14 py-10'>
+                    {["/Ecommerce accesories.png", "/Ecommerce accesories (1).png", "/Ecommerce accesories.png", "/Ecommerce accesories (3).png"].map((src, index) => (
+                        <Image key={index} src={src} alt="Product Image" width={300} height={363} className="w-full h-auto" />
+                    ))}
                 </div>
-
 
                 {/* second shop  */}
-                <div className='flex gap-14 py-[90px]'>
-                    <div>
-                        <Image
-                            src="/Ecommerce accesories (7).png "
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
-                    <div>
-
-                        {/* c:\Users\wajiz.pk\Downloads\Ecommerce accesories (1).png */}
-                        <Image
-                            src="/Ecommerce accesories (8).png"
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src="/Ecommerce accesories (9).png"
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src="/Ecommerce accesories (10).png"
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
+                <div className='grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-14 py-10'>
+                    {["/Ecommerce accesories (7).png", "/Ecommerce accesories (8).png", "/Ecommerce accesories (9).png", "/Ecommerce accesories (10).png"].map((src, index) => (
+                        <Image key={index} src={src} alt="Product Image" width={300} height={363} className="w-full h-auto" />
+                    ))}
                 </div>
+
                 {/* third shop */}
-                <div className='flex gap-14 py-[90px]'>
-                    <div>
-                        <Image
-                            src="/Ecommerce accesories (4).png "
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
-                    <div>
-
-                        {/* c:\Users\wajiz.pk\Downloads\Ecommerce accesories (1).png */}
-                        <Image
-                            src="/Ecommerce accesories (5).png "
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src="/Ecommerce accesories (6).png"
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src="/Ecommerce accesories (3).png"
-                            alt="Phone Icon"
-                            width={300}
-                            height={363}
-                            className="max-w-full h-auto"
-                        />
-                    </div>
+                <div className='grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-14 py-10'>
+                    {["/Ecommerce accesories (4).png", "/Ecommerce accesories (5).png", "/Ecommerce accesories (6).png", "/Ecommerce accesories (3).png"].map((src, index) => (
+                        <Image key={index} src={src} alt="Product Image" width={300} height={363} className="w-full h-auto" />
+                    ))}
                 </div>
             </div>
 
-            {/* last  */}
-            {/*  <div className='py-[80px] px-[580px]'>
-                    <Image
-                        src="/imgesss.png"
-                        alt="Phone Icon"
-                        width={904}
-                        height={200}
-                        className="max-w-full h-auto"
-                    />
-                </div>
-                <div> <Image
-                    src="/footer.png"
-                    alt="Phone Icon"
-                    width={2120}
-                    height={532}
-                    className="max-w-full h-auto"
+            {/* last section */}
+            <div className='w-full py-10 flex justify-center'>
+                <Image
+                    src="/imgesss.png"
+                    alt="Promo Image"
+                    width={904}
+                    height={200}
+                    className="w-full max-w-[904px] h-auto"
                 />
-                </div> */}
-            <div className='h-[220px] w-full'>
-
-                <div className='py-[70px] px-[600px] '>
-                    <Image
-                        src="/imgesss.png"
-                        alt="Phone Icon"
-                        width={904}
-                        height={200}
-                        className="max-w-full h-auto"
-                    />
-                </div>
-
             </div>
 
-
-
-            <div className='h-[532px] w-full'>
+            <div className='w-full'>
                 <Image
                     src="/footer.png"
-                    alt="Phone Icon"
+                    alt="Footer Image"
                     width={2120}
                     height={532}
-                    className="max-w-full h-auto"
+                    className="w-full h-auto"
                 />
             </div>
-
-
-
-
         </div>
     )
 }
 
 export default Shopping
+
